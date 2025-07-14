f7c0730602979c20f250b81d7f58b1466a30a9dc
Alex Bepple <alex@bepple.de>
2025-07-14 22:16:56 +0200

    (read deps) $ sniff deps using same logic as `pkgx dev`

--- scripts/deps-as-plus-pkg.js
+++ scripts/deps-as-plus-pkg.js
@@ -1,14 +1,21 @@
 #!/usr/bin/env -S pkgx +deno@2 -- deno --allow-read
 
+import { Path } from 'https://raw.githubusercontent.com/pkgxdev/libpkgx/refs/tags/v0.21.0/mod.ts'
+import sniff from 'https://raw.githubusercontent.com/alexbepple/mash/refs/heads/main/lib/sniff.v1.8.1.ts'
 import * as r from 'jsr:@rambda/rambda'
-import { parse } from 'jsr:@std/yaml'
 
-const yaml = await Deno.readTextFile("pkgx.yaml")
+const mapPkgToString = pkg => r.pipe(
+  pkg,
+  pkg => `${pkg.project}@${pkg.constraint}`,
+  r.replace('@*', ''),
+  r.replace('@~', '@'),
+)
+
 r.pipe(
-  yaml,
-  parse,
-  r.prop('dependencies'),
-  r.split(' '),
+  await sniff(Path.cwd()),
+  r.prop('pkgs'),
+  // r.tap(console.log),
+  r.map(mapPkgToString),
   r.map(r.concat('+')),
   r.join(' '),
   console.log,
